026d166d12e197e0c1030ff19e192c91af235b9e
kylin <kylin5108@gmail.com>
2018-09-21 15:59:47 +0800

    add watermark and change image type to webp

--- resources/views/app/home.blade.php
+++ resources/views/app/home.blade.php
@@ -22,35 +22,35 @@
   <!-- picture 1----------------------------------->
   <div class="carousel-inner" role="listbox" id="picture_list">
     <div class="item active">
-      <a href="{{ route('picture','1') }}"><img class="lazy" src="http://ismta.oss-cn-shenzhen.aliyuncs.com/a1.jpg" alt="..."></a>
+      <a href="{{ route('picture','1') }}"><img class="lazy" src="http://ismta.oss-cn-shenzhen.aliyuncs.com/a1.jpg?x-oss-process=style/webp" alt="..."></a>
       <div class="carousel-caption">
         高清投影会议室
       </div>
     </div>
      <!-- picture 2----------------------------------->
     <div class="item">
-      <a href="{{ route('picture', '2') }}"><img class="lazy" src="http://ismta.oss-cn-shenzhen.aliyuncs.com/a2.jpg" alt="..."></a>
+      <a href="{{ route('picture', '2') }}"><img class="lazy" src="http://ismta.oss-cn-shenzhen.aliyuncs.com/a2.jpg?x-oss-process=style/webp" alt="..."></a>
       <div class="carousel-caption">
         酷炫DIY配置研究深度学习
       </div>
     </div>
      <!-- picture 3----------------------------------->
     <div class="item">
-      <a href="{{ route('picture', '3') }}"><img class="lazy" data-src="http://ismta.oss-cn-shenzhen.aliyuncs.com/a3.jpg" alt="..."></a>
+      <a href="{{ route('picture', '3') }}"><img class="lazy" data-src="http://ismta.oss-cn-shenzhen.aliyuncs.com/a3.jpg?x-oss-process=style/webp" alt="..."></a>
       <div class="carousel-caption">
        水质监测与智能鱼缸
       </div>
     </div>
      <!-- picture 4----------------------------------->
     <div class="item">
-      <a href="{{ route('picture', '4') }}"><img class="lazy" data-src="http://ismta.oss-cn-shenzhen.aliyuncs.com/a4.jpg" alt="..."></a>
+      <a href="{{ route('picture', '4') }}"><img class="lazy" data-src="http://ismta.oss-cn-shenzhen.aliyuncs.com/a4.jpg?x-oss-process=style/webp" alt="..."></a>
       <div class="carousel-caption">
         多屏协同LED显示
       </div>
     </div>
      <!-- picture 5----------------------------------->
      <div class="item">
-      <a href="{{ route('picture', '5') }}"><img class="lazy" data-src="http://ismta.oss-cn-shenzhen.aliyuncs.com/a5.jpg" alt="..."></a>
+      <a href="{{ route('picture', '5') }}"><img class="lazy" data-src="http://ismta.oss-cn-shenzhen.aliyuncs.com/a5.jpg?x-oss-process=style/webp" alt="..."></a>
       <div class="carousel-caption">
         台式机硬件资源丰富
       </div>
@@ -58,42 +58,42 @@
 
       <!-- picture 6----------------------------------->
       <div class="item">
-      <a href="{{ route('picture', '6') }}"><img class="lazy" data-src="http://ismta.oss-cn-shenzhen.aliyuncs.com/a6.jpg" alt="..."></a>
+      <a href="{{ route('picture', '6') }}"><img class="lazy" data-src="http://ismta.oss-cn-shenzhen.aliyuncs.com/a6.jpg?x-oss-process=style/webp" alt="..."></a>
       <div class="carousel-caption">
         嵌入式硬件资源丰富
       </div>
     </div>
        <!-- picture 7----------------------------------->
        <div class="item">
-      <a href="{{ route('picture', '7') }}"><img class="lazy" data-src="http://ismta.oss-cn-shenzhen.aliyuncs.com/a7.jpg" alt="..."></a>
+      <a href="{{ route('picture', '7') }}"><img class="lazy" data-src="http://ismta.oss-cn-shenzhen.aliyuncs.com/a7.jpg?x-oss-process=style/webp" alt="..."></a>
       <div class="carousel-caption">
         参考书籍资料丰富
       </div>
     </div>
         <!-- picture 8----------------------------------->
     <div class="item">
-      <a href="{{ route('picture', '8') }}"><img class="lazy" data-src="http://ismta.oss-cn-shenzhen.aliyuncs.com/a8.jpg" alt="..."></a>
+      <a href="{{ route('picture', '8') }}"><img class="lazy" data-src="http://ismta.oss-cn-shenzhen.aliyuncs.com/a8.jpg?x-oss-process=style/webp" alt="..."></a>
       <div class="carousel-caption">
         良好的学习环境
       </div>
     </div>
          <!-- picture 9----------------------------------->
     <div class="item">
-      <a href="{{ route('picture', '9') }}"><img class="lazy" data-src="http://ismta.oss-cn-shenzhen.aliyuncs.com/a9.jpg" alt="..."></a>
+      <a href="{{ route('picture', '9') }}"><img class="lazy" data-src="http://ismta.oss-cn-shenzhen.aliyuncs.com/a9.jpg?x-oss-process=style/webp" alt="..."></a>
       <div class="carousel-caption">
         智能车机械电子计算机设计
       </div>
     </div>
           <!-- picture 10----------------------------------->
     <div class="item">
-      <a href="{{ route('picture', '10') }}"><img class="lazy" data-src="http://ismta.oss-cn-shenzhen.aliyuncs.com/a10.jpg" alt="..."></a>
+      <a href="{{ route('picture', '10') }}"><img class="lazy" data-src="http://ismta.oss-cn-shenzhen.aliyuncs.com/a10.jpg?x-oss-process=style/webp" alt="..."></a>
       <div class="carousel-caption">
         3D打印DLP学习
       </div>
     </div>
            <!-- picture 11----------------------------------->
            <div class="item">
-      <a href="{{ route('picture', '11') }}"><img class="lazy" data-src="http://ismta.oss-cn-shenzhen.aliyuncs.com/a11.jpg" alt="..."></a>
+      <a href="{{ route('picture', '11') }}"><img class="lazy" data-src="http://ismta.oss-cn-shenzhen.aliyuncs.com/a11.jpg?x-oss-process=style/webp" alt="..."></a>
       <div class="carousel-caption">
         Laravel 人机交互系统编程
       </div>
