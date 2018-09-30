9c5c9872e6df3dce35ca82f3038c391f7b9a7e7f
kylin <kylin5108@gmail.com>
2018-09-30 13:19:27 +0800

    disable not in use route.

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,6 @@
 // Route::get('/{selectedPic?}', 'StaticPagesController@home')->name('home');
 Route::get('/', 'StaticPagesController@home')->name('home');
 
-Route::get('/carousel/map/to/{picNum?}', 'StaticPagesController@slidePhoto')->name('slide_photo');
+// Route::get('/carousel/map/to/{picNum?}', 'StaticPagesController@slidePhoto')->name('slide_photo');
 
-Route::get('picture/{pic}', 'StaticPagesController@pic_one')->name('picture');
+// Route::get('picture/{pic}', 'StaticPagesController@pic_one')->name('picture');
