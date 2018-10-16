518784f1bd6debbace1b444587c755e18e3f1d66
Your Name <you@example.com>
2018-10-16 13:48:58 +0000

    remove the qucode

--- resources/views/app/about.blade.php
+++ resources/views/app/about.blade.php
@@ -3,34 +3,7 @@
     <div class="text-center" style="background-image: url('/img/background03.jpg'); width:100%;background-repeat:no-repeat;">
          <h2>预备成员交流群</h2>
 
-         <img src="img/myqr10.gif" alt="" style="width: 500px">
-
+         {{-- <img src="img/myqr10.gif" alt="" style="width: 500px"> --}}
     </div>
 
 @stop
-
-{{-- try {
-    $sms->send(13428136726, [
-        'content'  => '【ISMTA实验室】您的验证码是1234。如非本人操作，请忽略本短信',
-    ]);
-} catch (\Overtrue\EasySms\Exceptions\NoGatewayAvailableException $exception) {
-    $message = $exception->getException('yunpian')->getMessage();
-    dd($message);
-}
-
-
-
-【ISMTA实验室】亲，您好！有name订单，用户名：username，手机号：13428136726,请尽快联系。
-
-$user_phone=['13712806242','15625507322'];
-foreach($user_phone as $phone){
-try {
-    $sms->send($phone, [
-        'content'  => '【ISMTA实验室】亲，您好！有群订单，用户名：ismta.cn，手机号：* , 请尽快联系。',
-    ]);
-} catch (\Overtrue\EasySms\Exceptions\NoGatewayAvailableException $exception) {
-    $message = $exception->getException('yunpian')->getMessage();
-    dd($message);
-}
-}
- --}}
