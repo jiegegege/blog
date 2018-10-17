8df7afa63088ba1503235f27ff8751a3fb39a8db
Your Name <you@example.com>
2018-10-17 08:30:50 +0000

    add the img and video

--- resources/views/app/about.blade.php
+++ resources/views/app/about.blade.php
@@ -1,9 +1,10 @@
 @extends('layouts.default')
 @section('content')
-    <div class="text-center" style="background-image: url('/img/background03.jpg'); width:100%;background-repeat:no-repeat;">
+{{--     <div class="text-center" style="background-image: url('/img/background03.jpg'); width:100%;background-repeat:no-repeat;">
          <h2>预备成员交流群</h2>
 
-         {{-- <img src="img/myqr10.gif" alt="" style="width: 500px"> --}}
-    </div>
+         <img src="img/myqr10.gif" alt="" style="width: 500px">
+    </div> --}}
+    <img src="/img/background.jpg" alt="" width="100%">
 
 @stop
